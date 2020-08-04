4f69671f7532c654cea96e769c32dde02cfd77d0
Chiratna <prinusuva7876@gmail.com>
2020-08-04 12:59:52 +0530

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      statusBarBrightness: Brightness.light,
+      statusBarBrightness: Brightness.dark,
       statusBarColor: Colors.black,
     ));
     return ChangeNotifierProvider(
